a626dec4b156e71ca1f6af26d12ae3532982cae2
Ugur Tigu <ugurtigu@gmail.com>
2021-03-03 09:22:18 +0100

    first commit

---
+++ src/components/Login.vue
@@ -0,0 +1,9 @@
+<template>
+  <p>hello login</p>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <p><Login /></p>
   </div>
 </template>
+
+<script>
+import Login from '../components/Login';
+export default {
+  name: 'About',
+  components: {
+    Login,
+  },
+};
+</script>
+
+<style></style>
